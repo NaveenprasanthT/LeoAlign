6f9382adf2b5730a8ea94c79038c9e195a41be91
Social Agent <tech@socialagent.in>
2023-06-22 17:06:45 +0530

    gtag added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,50 @@
 import Footer from '@/Components/Footer'
 import Navbar from '@/Components/Navbar'
 import '@/styles/globals.css'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from "gtag"
+
 
 export default function App({ Component, pageProps }) {
-  return(
-  <>
-    <Navbar />
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)}
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
+
+
