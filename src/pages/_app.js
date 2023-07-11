b7c5aad831a3300bcb85f48a7c39cf51693906cb
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-07-11 15:23:47 +0530

    gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,9 @@ import Footer from '@/Components/Footer'
 import Navbar from '@/Components/Navbar'
 import '@/styles/globals.css'
 import { useRouter } from 'next/router'
-import Script from 'next/script'
 import { useEffect } from 'react'
 import * as gtag from "gtag"
-
+import Script from "next/script"
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
@@ -22,27 +21,16 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-      <Script type="text/javascript" id="zsiqchat" dangerouslySetInnerHTML={{
-        __html: `var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "191a01f82c6b91face6c6a1743caca14500873f76812a96ee4d481f4f2073cb6", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.in/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);`
-      }} />
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-QGNX4Z4Z06"></Script>
+          <Script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-QGNX4Z4Z06');
+            `}
+      </Script>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
